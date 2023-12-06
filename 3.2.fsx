1055fcb8a422974799f30357c086b6d0a82c8073
William Bergqvist <shazmodan@gmail.com>
2023-12-06 21:51:05 +0100

    3.2 Work in progress

--- 3.2.fsx
+++ 3.2.fsx
@@ -4,40 +4,84 @@ let readInput () = System.IO.File.ReadLines("3.txt")
 
 type Pos = { X: int; Y: int; Symbol: string }
 
-let isAdjacentToSymbol (schematic: string array) (numberMinX: int) (numberMaxX: int) (numberY: int) =
+type Direction =
+    | Left
+    | Right
+
+let getNumbersAdjacentToGearSymbol (schematic: string array) (gearX: int) (gearY: int) =
     let rowMax = schematic[0].Length - 1
     let columnMax = schematic.Length - 1
 
-    let isSymbol (c: char) = 
-        let a = not (System.Char.IsAsciiDigit c) && c <> '.'
-        // printfn "%A" {| Char = c; IsSymbol = a |} //TODO:REMOVE
-        a
-
     let isWithinXBounds (index: int) = not (index < 0) && not (index > rowMax)
     let isWithinYBounds (index: int) = not (index < 0) && not (index > columnMax)
+    let isNumber (c: char) = System.Char.IsAsciiDigit c
+
+    let rec parseNumberDirection (x: int) (y: int) (direction: Direction) (acc: char list) =
+        if isWithinXBounds x && isWithinYBounds y then
+            let value = schematic[y][x]
+            let isDigit = System.Char.IsAsciiDigit value
+            
+            match isDigit with
+            | true -> 
+                let nextX = 
+                    match direction with
+                    | Direction.Left -> x - 1
+                    | Direction.Right -> x + 1
+                
+                let newAcc = value :: acc
+                
+                parseNumberDirection nextX y direction newAcc
+            | false -> // terminal condition
+                match direction with
+                | Direction.Left -> acc
+                | Direction.Right -> acc |> List.rev
+        else // terminal condition
+            match direction with
+            | Direction.Left -> acc
+            | Direction.Right -> acc |> List.rev
+
+    let parseLeft (x: int) (y: int) =  parseNumberDirection x y Direction.Left []
+    let parseRight (x: int) (y: int) = parseNumberDirection x y Direction.Right []
 
     let leftCell = 
-        let x = max (numberMinX - 1) 0
-        isSymbol (schematic[numberY][x])
-    
+        let x = max (gearX - 1) 0
+        parseLeft x gearY
+
     let rightCell = 
-        let x = min (numberMaxX + 1) (schematic[0].Length - 1)
-        isSymbol (schematic[numberY][x])
+        let x = min (gearX + 1) (schematic[0].Length - 1)
+        parseRight x gearY
 
     let topCells = 
-        match numberY - 1 with
+        match gearY - 1 with
         | y when isWithinYBounds y ->
-            [ numberMinX-1..numberMaxX+1 ]
-            |> List.filter isWithinXBounds
-            |> List.exists(fun x -> isSymbol (schematic[y][x]))
-        | _ -> false
+            let rightAbove = schematic[y][gearX]
 
+            match isNumber rightAbove with
+            | true ->
+                let parsedLeft = parseLeft (gearX-1) y //parseNumberDirection (gearX-1) y Direction.Left []
+                let parsedRight = parseRight (gearX+1) y //parseNumberDirection (gearX+1) y Direction.Right []
+                
+                let completeNumberChar = parsedLeft @ [rightAbove] @ parsedRight
+                let number = System.String.Join("", completeNumberChar) |> int
+
+                [ number ]
+            | false ->
+                let parsedAboveLeft = parseLeft (gearX-1) (gearY-1) 
+                let parsedAboveRight = parseRight (gearX+1) (gearY+1)
+                
+                let numberAboveLeft = System.String.Join("", parsedAboveLeft) |> int
+                let numberAboveRight = System.String.Join("", parsedAboveRight) |> int
+
+                [ numberAboveLeft; numberAboveRight ]
+        | _ -> []
+
+    //TODO: Just do like topCells do 
     let bottomCells =
-        match numberY + 1 with
+        match gearY + 1 with
         | y when isWithinYBounds y ->
-            [ numberMinX-1..numberMaxX+1 ]
+            [ gearMinX-1..gearMaxX+1 ]
             |> List.filter isWithinXBounds
-            |> List.exists(fun x -> isSymbol (schematic[y][x]))
+            |> List.exists(fun x -> isNumber (schematic[y][x]))
         | _ -> false
 
     [leftCell; rightCell; topCells; bottomCells] |> List.exists id
@@ -81,8 +125,11 @@ let testData = [|
     ".664.598.."
 |]
 
-//testData |> solve
+let result = testData |> solve
 
-readInput ()
-|> Seq.toArray
-|> solve
+result
+result = 467835
+
+// readInput ()
+// |> Seq.toArray
+// |> solve
