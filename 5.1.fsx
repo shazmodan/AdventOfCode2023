ecc26e5ca7eaa1e1c952ad8a3e2661a502017dab
William Bergqvist <shazmodan@gmail.com>
2023-12-14 22:18:05 +0100

    5.1 wip

--- 5.1.fsx
+++ 5.1.fsx
@@ -1,3 +1,10 @@
+open System.Text.RegularExpressions
+
+let (|Regex|_|) pattern input =
+    let m = Regex.Match(input, pattern)
+    if m.Success then Some(List.tail [ for g in m.Groups -> g.Value ])
+    else None
+
 let log a =
     printfn "%A" a
     a
@@ -25,15 +32,54 @@ type Maps =
           TemperatureToHumidityMap = []
           HumidityToLocationMap = [] }
 
-let parseInputIntoMaps (input: string seq) =
+let parseInputIntoMaps (input: string list) =
+    let maps = 
+        let row = List.head input
+        let numbers = 
+            row.Substring(row.IndexOf("seeds: ")).Split(" ")
+            |> Array.map int
+            |> Array.toList
+        
+        { Maps.Default with Seeds = numbers }
 
+    let rec parseLines (activeMap: string) (input: string list) (acc: Maps) =
+        match List.tryHead input with
+        | None -> acc // base case
+        | Some head ->
+            let newAcc =
+                let getMapping (head: string) : Mapping = 
+                    match (head.Split( " ") |> Array.toList) with
+                    | [ toNr; fromNr; rangeNr ] -> { From = int fromNr; To = int toNr ; Range = int rangeNr }
+                    | x -> failwithf "Failed to create mapping with: %A" x
+
+                match activeMap with
+                | "" -> acc
+                | "seed-to-soil map:" -> { acc with SeedToSoilMap = getMapping head :: acc.SeedToSoilMap }
+                | "soil-to-fertilizer map:" -> { acc with SoilToFertilizerMap =  getMapping head :: acc.SoilToFertilizerMap }
+                | "fertilizer-to-water map:" -> { acc with FertilizerToWaterMap =  getMapping head :: acc.FertilizerToWaterMap }
+                | "water-to-light map:" -> { acc with WaterToLightMap =  getMapping head :: acc.WaterToLightMap }
+                | "light-to-temperature map:" -> { acc with LightToTemperatureMap =  getMapping head :: acc.LightToTemperatureMap }
+                | "temperature-to-humidity map:" -> { acc with TemperatureToHumidityMap =  getMapping head :: acc.TemperatureToHumidityMap }
+                | "humidity-to-location map:" -> { acc with HumidityToLocationMap =  getMapping head :: acc.HumidityToLocationMap }
+                | x -> failwithf "Failed to parse activeMap: %s" x
+            
+            match head.[0] with
+            | char when System.Char.IsAsciiLetter(char) ->
+                // time to set a new activeMap
+                parseLines head (List.tail input) newAcc
+            | char when System.Char.IsControl(char) ->
+                //newline character discovered
+                parseLines "" (List.tail input) newAcc
+            | char when System.Char.IsDigit(char) -> 
+                parseLines activeMap (List.tail input) newAcc
+            | x -> failwithf "Failed to match head.[0]: %c" x
+    
+    parseLines "" (List.tail input) maps
 
 let solve (input: string seq) =
-    (Maps.Default, input)
-    ||> Seq.fold
-            (fun acc curr -> 
-                
-            )
+    let maps = parseInputIntoMaps (input |> Seq.toList)
+
+    maps
 
 
 let testData =
@@ -69,4 +115,7 @@ let testData =
 
       "humidity-to-location map:"
       "60 56 37"
-      "56 93 4" ]
+      "56 93 4" 
+    ]
+
+testData |> solve
